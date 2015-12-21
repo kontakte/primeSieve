2d6eb8585776ce95d5c1a5b7699ae0847bae33d1
kontakte <lintakte@gmail.com>
2015-12-21 14:07:23 -0500

    Added 'chunking' to eliminate memory errors in large sieves. This does not eliminate memory errors with large sets of primes

--- primeSieve.py
+++ primeSieve.py
@@ -1,22 +1,48 @@
 import sys
 
+STEP = 1000
+
 results = []
 lastResult = None
 
 max = int(sys.argv[1])
 sieve = []
-for i in range(2,max):
-  sieve.append(i)
 
-while len(sieve) >= 1:
-  lastResult = sieve[0]
-  newSieve = []
-  print("Sieving %d" % lastResult)
-  for i in sieve:
-    if i % lastResult != 0:
-      newSieve.append(i)
-  results.append(lastResult)
-  sieve = newSieve
+lastMax = 2
+currentMax = max if max < STEP else STEP
 
-for i in results:
-  print i
+while currentMax <= max:
+  print("Working on %d - %d" % (lastMax, currentMax))
+  
+  for i in range(lastMax,currentMax):
+    sieve.append(i)
+
+  print("Removing multiples of known primes")
+  for prime in results:
+    #print("Removing multiples of %d from sieve" % prime)
+    newSieve = []
+    for i in sieve:
+      if i % prime != 0:
+        newSieve.append(i)
+    sieve = newSieve
+
+  print("Sieving")
+  while len(sieve) >= 1:
+    lastResult = sieve[0]
+    newSieve = []
+    for i in sieve:
+      if i % lastResult != 0:
+        newSieve.append(i)
+    results.append(lastResult)
+    sieve = newSieve
+
+  if currentMax == max:
+    break
+  
+  lastMax = currentMax
+  currentMax += STEP
+  if currentMax > max:
+    currentMax = max
+
+#for i in results:
+#  print i
